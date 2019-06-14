122ed194c4d11dc43013af1871a02aa7bb13d291
duxuefeng <1395275016@qq.com>
2019-06-14 17:12:09 +0800

    fix pinyin

--- app/Tasks.php
+++ app/Tasks.php
@@ -21,14 +21,14 @@ class Tasks extends Model
     );
 
     const FENLEI_ZHUOYOU = 1;
-    const FENLEI_DITIE = 2;
+    const FENLEI_DIBIAO = 2;
     const FENLEI_GUANGJIE = 3;
     const FENLEI_WANGYOU = 4;
     const FENLEI_DIANYING = 5;
 
     public static $categorys = array(
         self::FENLEI_ZHUOYOU => '桌游',
-        self::FENLEI_DITIE => '地标打卡',
+        self::FENLEI_DIBIAO => '地标打卡',
         self::FENLEI_GUANGJIE => '逛街',
         self::FENLEI_WANGYOU => '线上网游',
         self::FENLEI_DIANYING => '电影',
