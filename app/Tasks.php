c3ae142f41614ee4b80325ec65d53f7cb65ac7d3
duxuefeng <1395275016@qq.com>
2019-06-14 16:50:33 +0800

    1 task 枚举值 2 git 忽略文件

--- app/Tasks.php
+++ app/Tasks.php
@@ -10,6 +10,30 @@ class Tasks extends Model
     //
     const TABLE = 'buxian_tasks';
 
+    const SEX_NO = 0;
+    const SEX_MALE = 1;
+    const SEX_FEMALE = 2;
+
+    public static $sexs = array(
+        self::SEX_NO => '不限制',
+        self::SEX_MALE => '男',
+        self::SEX_FEMALE => '女',
+    );
+
+    const FENLEI_ZHUOYOU = 1;
+    const FENLEI_DITIE = 2;
+    const FENLEI_GUANGJIE = 3;
+    const FENLEI_WANGYOU = 4;
+    const FENLEI_DIANYING = 5;
+
+    public static $categorys = array(
+        self::FENLEI_ZHUOYOU => '桌游',
+        self::FENLEI_DITIE => '地标打卡',
+        self::FENLEI_GUANGJIE => '逛街',
+        self::FENLEI_WANGYOU => '线上网游',
+        self::FENLEI_DIANYING => '电影',
+    );
+
     static public function getTaskList($count = false, $makePage = false, $page = 1, $pageSize = 10)
     {
         if ($makePage) {
@@ -30,6 +54,14 @@ class Tasks extends Model
 
         return false;
     }
-    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'content', 'category','sex','min_age','max_age','valid_at','status'
+    ];
 
 }
