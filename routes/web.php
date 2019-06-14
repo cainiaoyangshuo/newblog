c4442b4de4757bc98e6c25eeeb263ebfebad1488
tony <3546544379@qq.com>
2019-06-14 20:48:41 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -69,11 +69,16 @@ Route::group(['prefix' => 'dashboard', 'middleware' => ['auth', 'admin']], funct
 
 // Article
 Route::get('/article', 'ArticleController@index');
-Route::get('/', function (){
-    return View::make('index');
-});
+//Route::get('/', function (){
+//    return View::make('index');
+//});
 Route::get('{slug}', 'ArticleController@show');
+
 
 
 Route::get('/task', 'TasksController@index');
 Route::get('/user', 'UserController@index');
+
+Route::get('/', 'TasksController@index');
+Route::get('/detail', 'TasksController@detail');
+
